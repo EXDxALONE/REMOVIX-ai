f3cf190379d41f3963f8ac8bd5b857d81c026fcb
Rishiraj <rishiraj10b28@gmail.com>
2026-04-13 23:46:29 +0530

    Fix Vercel fetch failed error and supabase environment variables

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL || "https://rutgshaiepkulvtvbely.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ1dGdzaGFpZXBrdWx2dHZiZWx5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1MDUwOTMsImV4cCI6MjA4OTA4MTA5M30.EARa2kbAoV7TZBgrd1zrCHsamI-VtgC2jHstxKCm4ds";
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
